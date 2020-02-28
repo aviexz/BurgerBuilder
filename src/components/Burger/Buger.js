1d994d0fca3a1de7379fd4087ff0febfdf168a4a
aviexz <alexandroivaldez@gmail.com>
2020-02-27 23:43:56 -0600

    Added a constant named transformedIngredients that outputs <BurgerIngredient /> components. Should be dynamic in the near future.

--- src/components/Burger/Buger.js
+++ src/components/Burger/Buger.js
@@ -3,11 +3,23 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngrediant/BurgerIngrediant";
 
 const burger = props => {
+  //Props is passing an object NOT an array, so map cannot be used.
+
+  //This will extract the keys of a given object and turn it into an array, so we can use map.
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    ingredientKey => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return (
+          <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+        );
+      });
+    }
+  );
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
